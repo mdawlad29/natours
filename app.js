85b529938d32dd056c75691c61ac524794ea54bb
mdawlad29 <mdawladbdinfo@gmail.com>
2024-05-25 18:10:22 +0600

    Single product Get

--- app.js
+++ app.js
@@ -16,6 +16,22 @@ app.get("/api/v1/tours", (req, res) => {
   });
 });
 
+app.get("/api/v1/tours/:id", (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(400).json({
+      status: "Failed",
+      message: "Invalid ID",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: tour,
+  });
+});
+
 app.post("/api/v1/tour", (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
