51773b19aaac6f74dc5046acccd5bffdafcb37d7
mdawlad29 <mdawladbdinfo@gmail.com>
2024-05-25 19:37:51 +0600

    Create Middleware

--- app.js
+++ app.js
@@ -5,11 +5,22 @@ const port = 8000;
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log("Hello from the middleware!");
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 const tours = JSON.parse(fs.readFileSync(`./data/tours-sample.json`));
 
 const getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
+    requestTedAt: req.requestTime,
     result: tours.length,
     data: tours,
   });
