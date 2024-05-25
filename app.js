2df2666f1878bc047ac0d5bdc3cc3a742fa84432
mdawlad29 <mdawladbdinfo@gmail.com>
2024-05-25 17:44:52 +0600

    Get all json file

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 const port = 8000;
 
-app.get("/", (req, res) => {
+const tours = JSON.parse(fs.readFileSync(`./data/tours-sample.json`));
+
+app.get("/api/v1/tours", (req, res) => {
   res.status(200).json({
     message: "Hello from the server!",
-    app: "Natours",
+    status: 200,
+    result: tours.length,
+    data: tours,
   });
 });
 
